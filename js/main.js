f9499d6b7155c4f55d50336a9ca638bb79c9e0f6
nicoCT04 <nicoconcua@gmail.com>
2025-05-29 13:42:27 -0600

    Estructure of the project

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const btnMenu = document.getElementById('btn-menu');
-const navMenu = document.getElementById('nav-menu');
-
-btnMenu.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-});
